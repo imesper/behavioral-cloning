1214ca1e7d39f7a14c0345445572003b704ea4b7
Ian Esper <imesper@gmail.com>
2018-05-30 18:15:01 -0300

    Writeup and refactoring

--- model.py
+++ model.py
@@ -1,95 +1,56 @@
-import cv2
-import csv
-import os
 import sklearn
 from sklearn.model_selection import train_test_split
 from keras import Sequential, optimizers
 from keras.callbacks import ModelCheckpoint
-from keras.optimizers import Adam 
+from keras.optimizers import Adam
 from keras.layers import Flatten, Dense, Conv2D, Lambda, Cropping2D, Dropout, LeakyReLU
-from random import shuffle
+from random import shuffle, randint
 import numpy as np
+import cv2
+import csv
+import os
 
-samples = []
-with open('../DrivingData/driving_log.csv') as csvfile:
-    reader = csv.reader(csvfile)
-    for line in reader:
-        path = line[0]
-        path_l = line[1]
-        path_r = line[2]
-        filename = path.split('/')[-1]
-        filename_l = path_l.split('/')[-1]
-        filename_r = path_r.split('/')[-1]
-        line[0] = '../DrivingData/IMG/'+filename
-        line[1] = '../DrivingData/IMG/'+filename_l
-        line[2] = '../DrivingData/IMG/'+filename_r
-        #print(line)
-        samples.append(line)
-
-with open('../DrivingData03/driving_log.csv') as csvfile:
-    reader = csv.reader(csvfile)
-    for line in reader:
-        path = line[0]
-        path_l = line[1]
-        path_r = line[2]
-        filename = path.split('/')[-1]
-        filename_l = path_l.split('/')[-1]
-        filename_r = path_r.split('/')[-1]
-        line[0] = '../DrivingData03/IMG/'+filename
-        line[1] = '../DrivingData03/IMG/'+filename_l
-        line[2] = '../DrivingData03/IMG/'+filename_r
-        #print(line)
-        samples.append(line)
-
-with open('../DrivingData02/driving_log.csv') as csvfile:
-    reader = csv.reader(csvfile)
-    for line in reader:
-        path = line[0]
-        path_l = line[1]
-        path_r = line[2]
-        filename = path.split('/')[-1]
-        filename_l = path_l.split('/')[-1]
-        filename_r = path_r.split('/')[-1]
-        line[0] = '../DrivingData02/IMG/'+filename
-        line[1] = '../DrivingData02/IMG/'+filename_l
-        line[2] = '../DrivingData02/IMG/'+filename_r
-        samples.append(line)
-
-with open('data/driving_log.csv') as csvfile:
-    reader = csv.reader(csvfile)
-    for line in reader:
-        samples.append(line)
-
-samples  = sklearn.utils.shuffle(samples)
-#images = []
-#measurements = []
-#for line in samples:
-#    file_path = line[0]
-#    image = cv2.imread(file_path)
-#    images.append(image)
-#    measurement = float(line[3])
-#    measurements.append(measurement)
-#    image_flipped = np.fliplr(image)
-#    images.append(image_flipped)
-#    measurement_flipped = -measurement
-#    measurements.append(measurement_flipped)
+# Load data
+# '../DrivingData02/
 
 
-##train_samples, validation_samples, train_meas, validation_meas = train_test_split(images, measurements, test_size=0.2)
-train_samples, validation_samples = train_test_split(samples, test_size=0.2)
-# import cv2
-# import numpy as np
-# import sklearn
-import random
+def loadData(path, changePathinCSV=False):
+    samples = []
+    if changePathinCSV:
+        with open(path + 'driving_log.csv') as csvfile:
+            reader = csv.reader(csvfile)
+            for line in reader:
+                path = line[0]
+                path_l = line[1]
+                path_r = line[2]
+                filename = path.split('/')[-1]
+                filename_l = path_l.split('/')[-1]
+                filename_r = path_r.split('/')[-1]
+                line[0] = path + 'IMG/' + filename
+                line[1] = path + 'IMG/' + filename_l
+                line[2] = path + 'IMG/' + filename_r
+                samples.append(line)
+    else:
+        with open(path + 'driving_log.csv') as csvfile:
+            reader = csv.reader(csvfile)
+            for line in reader:
+                samples.append(line)
+
+    samples = sklearn.utils.shuffle(samples)
+    return samples
+
+
 def randomDarkener(image):
-    alpha = random.randint(0,2) 
-    beta = random.randint(-30,0)
-    res = cv2.addWeighted(image, alpha, np.zeros(image.shape, image.dtype), 0, beta)
+    alpha = 1
+    beta = randint(-30, 0)
+    res = cv2.addWeighted(image, alpha, np.zeros(
+        image.shape, image.dtype), 0, beta)
     return res
+
 
 def generator(samples, batch_size=32):
     num_samples = len(samples)
-    while 1: # Loop forever so the generator never terminates
+    while 1:  # Loop forever so the generator never terminates
         shuffle(samples)
         for offset in range(0, num_samples, batch_size):
             batch_samples = samples[offset:offset+batch_size]
@@ -100,10 +61,11 @@ def generator(samples, batch_size=32):
                 for i in range(3):
                     name = batch_sample[i].strip()
                     if os.path.isfile(name):
-                        image = cv2.cvtColor(cv2.imread(name),cv2.COLOR_BGR2RGB)
+                        image = cv2.cvtColor(
+                            cv2.imread(name), cv2.COLOR_BGR2RGB)
                     else:
                         image = cv2.imread(name)
-                        exit(1)   
+                        exit(1)
                     angle = float(batch_sample[3])
                     if i == 1:
                         angle += 0.20
@@ -116,48 +78,77 @@ def generator(samples, batch_size=32):
                         images.append(image_flipped)
                         measurement_flipped = -angle
                         angles.append(measurement_flipped)
-                    if random.randint(0, 1000) % 30 == 0:
-                       image = randomDarkener(image)
-                       images.append(image)
-                       angles.append(angle)
+                    if randint(0, 1000) % 30 == 0:
+                        image = randomDarkener(image)
+                        images.append(image)
+                        angles.append(angle)
 
             # trim image to only see section with road
             X_train = np.array(images)
             y_train = np.array(angles)
             yield sklearn.utils.shuffle(X_train, y_train)
 
-batch_size = 32
-train_generator = generator(train_samples, batch_size=batch_size)
-validation_generator = generator(validation_samples, batch_size=batch_size)
-model = Sequential()
-model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160, 320, 3)))
-model.add(Cropping2D(cropping=((60, 25), (0, 0))))
-model.add(Conv2D(24, (5, 5), strides=(2, 2)))
-model.add(LeakyReLU(alpha=.01))
-model.add(Conv2D(36, (5, 5), strides=(2, 2)))
-model.add(LeakyReLU(alpha=.01))
-model.add(Conv2D(48, (5, 5), strides=(2, 2)))
-model.add(LeakyReLU(alpha=.01))
-model.add(Dropout(0.5))
-model.add(Conv2D(64, (3, 3)))
-model.add(LeakyReLU(alpha=.01))
-model.add(Conv2D(64, (3, 3)))
-model.add(LeakyReLU(alpha=.01))
-model.add(Dropout(0.5))
-model.add(Flatten())
-#model.add(Dropout(0.8))
-model.add(Dropout(0.5))
-#model.add(Dense(400))
-model.add(Dense(100))
-model.add(Dense(50))
-model.add(Dense(10))
-model.add(Dense(1))
-checkpoint = ModelCheckpoint('model-{epoch:03d}.h5',
+
+def CNN(train_samples, validation_samples, batch_size):
+
+    model = Sequential()
+    model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160, 320, 3)))
+    model.add(Cropping2D(cropping=((60, 25), (0, 0))))
+    model.add(Conv2D(24, (5, 5), strides=(2, 2)))
+    model.add(LeakyReLU(alpha=.01))
+    model.add(Conv2D(36, (5, 5), strides=(2, 2)))
+    model.add(LeakyReLU(alpha=.01))
+    model.add(Conv2D(48, (5, 5), strides=(2, 2)))
+    model.add(LeakyReLU(alpha=.01))
+    model.add(Dropout(0.5))
+    model.add(Conv2D(64, (3, 3)))
+    model.add(LeakyReLU(alpha=.01))
+    model.add(Conv2D(64, (3, 3)))
+    model.add(LeakyReLU(alpha=.01))
+    model.add(Dropout(0.5))
+    model.add(Flatten())
+    model.add(Dropout(0.5))
+    model.add(Dense(100))
+    model.add(Dense(50))
+    model.add(Dense(10))
+    model.add(Dense(1))
+    model.summary()
+
+    return model
+
+
+def main():
+    samples = []
+    # Loading Data from 3 different folders
+    # Each folder has different runs on simulator
+    samples += loadData('../DrivingData/')
+    samples += loadData('../DrivingData02/')
+    samples += loadData('data/')
+    print(len(samples))
+    # Spliting the data between trainnig (80%) and validation (20%)
+    train_samples, validation_samples = train_test_split(
+        samples, test_size=0.2)
+
+    # Setting the batch size
+    batch_size = 32
+    # Getting the model
+    model = CNN(train_samples, validation_samples, batch_size)
+
+    # Running the model, saving only the best models based on validation loss
+    checkpoint = ModelCheckpoint('model-{epoch:03d}.h5',
                                  monitor='val_loss',
                                  verbose=0,
                                  save_best_only=True,
                                  mode='auto')
 
-model.compile(loss='mse', optimizer=Adam(lr=1.0e-4))
+    model.compile(loss='mse', optimizer=Adam(lr=1.0e-4))
 
-model.fit_generator(train_generator, steps_per_epoch=len(train_samples)/batch_size, validation_data=validation_generator, validation_steps=len(validation_samples)/batch_size, epochs=30, callbacks=[checkpoint])
+    train_generator = generator(train_samples, batch_size=batch_size)
+    validation_generator = generator(validation_samples, batch_size=batch_size)
+
+    model.fit_generator(train_generator, steps_per_epoch=len(train_samples)/batch_size, validation_data=validation_generator,
+                        validation_steps=len(validation_samples)/batch_size, epochs=30, callbacks=[checkpoint])
+
+
+if __name__ == '__main__':
+    main()
--- writeup_template.md
+++ writeup_template.md
@@ -1,20 +1,20 @@
 # **Behavioral Cloning** 
 
-## Writeup Template
 
-### You can use this file as a template for your writeup if you want to submit it as a markdown file, but feel free to use some other method and submit a pdf if you prefer.
+
+### Deep Learning Network for cloning driving behavior.
 
 ---
 
 **Behavioral Cloning Project**
 
 The goals / steps of this project are the following:
+
 * Use the simulator to collect data of good driving behavior
 * Build, a convolution neural network in Keras that predicts steering angles from images
 * Train and validate the model with a training and validation set
 * Test that the model successfully drives around track one without leaving the road
 * Summarize the results with a written report
-
 
 [//]: # (Image References)
 
@@ -28,35 +28,89 @@ The goals / steps of this project are the following:
 
 ## Rubric Points
 ### Here I will consider the [rubric points](https://review.udacity.com/#!/rubrics/432/view) individually and describe how I addressed each point in my implementation.  
-
 ---
+
 ### Files Submitted & Code Quality
 
 #### 1. Submission includes all required files and can be used to run the simulator in autonomous mode
 
 My project includes the following files:
+
 * model.py containing the script to create and train the model
 * drive.py for driving the car in autonomous mode
 * model.h5 containing a trained convolution neural network 
-* writeup_report.md or writeup_report.pdf summarizing the results
+* writeup_report.md summarizing the results
 
 #### 2. Submission includes functional code
+
 Using the Udacity provided simulator and my drive.py file, the car can be driven autonomously around the track by executing 
+
 ```sh
 python drive.py model.h5
 ```
 
+
 #### 3. Submission code is usable and readable
 
-The model.py file contains the code for training and saving the convolution neural network. The file shows the pipeline I used for training and validating the model, and it contains comments to explain how the code works.
+The model.py file contains the code for training and saving the convolution neural network. The file shows the pipeline I used for training and validating the model, and it contains comments to explain how the code works. 
+
 
 ### Model Architecture and Training Strategy
 
 #### 1. An appropriate model architecture has been employed
 
-My model consists of a convolution neural network with 3x3 filter sizes and depths between 32 and 128 (model.py lines 18-24) 
+My model consists of a convolution neural network with 5x5 and 3x3 kernel sizes and depths between 24 and 64. The complete network summary is shown below.
 
-The model includes RELU layers to introduce nonlinearity (code line 20), and the data is normalized in the model using a Keras lambda layer (code line 18). 
+```sh
+_________________________________________________________________
+Layer (type)                 Output Shape              Param #
+=================================================================
+lambda_1 (Lambda)            (None, 160, 320, 3)       0
+_________________________________________________________________
+cropping2d_1 (Cropping2D)    (None, 75, 320, 3)        0
+_________________________________________________________________
+conv2d_1 (Conv2D)            (None, 36, 158, 24)       1824
+_________________________________________________________________
+leaky_re_lu_1 (LeakyReLU)    (None, 36, 158, 24)       0
+_________________________________________________________________
+conv2d_2 (Conv2D)            (None, 16, 77, 36)        21636
+_________________________________________________________________
+leaky_re_lu_2 (LeakyReLU)    (None, 16, 77, 36)        0
+_________________________________________________________________
+conv2d_3 (Conv2D)            (None, 6, 37, 48)         43248
+_________________________________________________________________
+leaky_re_lu_3 (LeakyReLU)    (None, 6, 37, 48)         0
+_________________________________________________________________
+dropout_1 (Dropout)          (None, 6, 37, 48)         0
+_________________________________________________________________
+conv2d_4 (Conv2D)            (None, 4, 35, 64)         27712
+_________________________________________________________________
+leaky_re_lu_4 (LeakyReLU)    (None, 4, 35, 64)         0
+_________________________________________________________________
+conv2d_5 (Conv2D)            (None, 2, 33, 64)         36928
+_________________________________________________________________
+leaky_re_lu_5 (LeakyReLU)    (None, 2, 33, 64)         0
+_________________________________________________________________
+dropout_2 (Dropout)          (None, 2, 33, 64)         0
+_________________________________________________________________
+flatten_1 (Flatten)          (None, 4224)              0
+_________________________________________________________________
+dropout_3 (Dropout)          (None, 4224)              0
+_________________________________________________________________
+dense_1 (Dense)              (None, 100)               422500
+_________________________________________________________________
+dense_2 (Dense)              (None, 50)                5050
+_________________________________________________________________
+dense_3 (Dense)              (None, 10)                510
+_________________________________________________________________
+dense_4 (Dense)              (None, 1)                 11
+=================================================================
+Total params: 559,419
+Trainable params: 559,419
+Non-trainable params: 0
+_________________________________________________________________
+```
+The model includes Leaky RELU layers to introduce nonlinearity and to prevent neurons to die, and the data is normalized in the model using a Keras lambda layer (code line 95). 
 
 #### 2. Attempts to reduce overfitting in the model
 
@@ -67,6 +121,7 @@ The model was trained and validated on different data sets to ensure that the mo
 #### 3. Model parameter tuning
 
 The model used an adam optimizer, so the learning rate was not tuned manually (model.py line 25).
+
 
 #### 4. Appropriate training data
 
